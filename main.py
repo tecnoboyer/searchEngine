21395b3b54abfe85c5ce7adbc761263b4abe9f1f
tecnoboyer <ingenieroboyer@gmail.com>
2025-02-01 10:09:31 -0500

    usingInstrospection

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 
-# Get the directory of the current file
+
 current_directory = os.path.dirname(os.path.abspath(__file__))
-
-# Construct the path to the API_KEY file in the parent directory
 api_key_file_path = os.path.abspath(os.path.join(current_directory, '..', 'API_KEY.txt'))
+EngineID_file_path = os.path.abspath(os.path.join(current_directory, '..', 'EngineID.txt'))
 
-# Open and read the API_KEY file
 api_key = open(api_key_file_path).read()
+EngineID= open(EngineID_file_path).read()
 
 print(api_key)
+print(EngineID)
